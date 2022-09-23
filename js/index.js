7ab3d407ab667c5fa201d03267f1fee62ac46f7d
Mark <metalwolfmk@gmail.com>
2022-09-23 15:52:47 +0700

    super design

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    var currentCycle = 1;
-    $('#entryPeriod').text('Entry Period (5 days): ' + GetDatePeriod(currentCycle)[0] + GetTimeByZone())
-    $('#resultsPeriod').text('Results Period (4 days): ' + GetDatePeriod(currentCycle)[1] + GetTimeByZone())
-    $("#countdown").countdown(GetDatePeriod(currentCycle)[2], function(event) {
-    $(this).text(
+    $('#entryPeriod').text(GetDatePeriod()[0])
+    $('#resultsPeriod').text(GetDatePeriod()[1])
+    $("#countdown").countdown(GetDatePeriod()[2], function(event) {
+    $(this).text(GetDatePeriod()[3] +
       event.strftime('%D days %H:%M:%S')
     );
     if(event.elapsed) {
@@ -12,9 +11,10 @@ $(document).ready(function() {
   });
 });  
 
-function GetDatePeriod(currentCycle) {
+function GetDatePeriod() {
     var currentDate = new Date();
     var dateAnchor = new Date(2022, 8, 11);
+    var currentCycle = Math.floor(dateDiffInDays(currentDate, dateAnchor)/9);
     dateAnchor.setDate(dateAnchor.getDate() + currentCycle*9)
     var dateStart = new Date(dateAnchor);
     var dateEntry = new Date(dateAnchor);
@@ -23,35 +23,55 @@ function GetDatePeriod(currentCycle) {
     dateEntry.setDate(dateStart.getDate() + 5);
     dateResults.setDate(dateEntry.getDate() + 4);
 
-    var strDateStart = formatDate(dateStart);
-    var strDateEntry = formatDate(dateEntry);
-    var strDateResults = formatDate(dateResults);
+    var strDateStart = formatDateNew(dateStart);
+    var strDateEntry = formatDateNew(dateEntry);
+    var strDateResults = formatDateNew(dateResults);
 
     var countdown = 0;
+    var countdownPeriod = 'Entry';
     if(currentDate.getTime() > dateStart.getTime() && currentDate.getTime() < dateEntry.getTime()) {
-        countdown = strDateEntry
+        countdown = formatDateOld(dateEntry);
+        countdownPeriod = 'Entry  period ends in ';
     }
 
     if(currentDate.getTime() > dateEntry.getTime() && currentDate.getTime() < dateResults.getTime()) {
-        countdown = strDateResults
+        countdown = formatDateOld(dateResults);
+        countdownPeriod = 'Results period ends in ';
     }
 
     return [
-        (strDateStart + GetTimeByZone() + ' - ' + strDateEntry),
-        (strDateEntry + GetTimeByZone() + ' - ' + strDateResults),
-        countdown
+        ('' + strDateStart + GetTimeByZone() + ' ➜ ' + strDateEntry + GetTimeByZone() + ''),
+        ('' + strDateEntry + GetTimeByZone() + ' ➜ ' + strDateResults + GetTimeByZone() + ''),
+        countdown,
+        countdownPeriod
     ];
 }
 
-function formatDate(date) {
+function formatDateOld(date) {
     return date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate();
 }
 
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+function formatDateNew(date) {
+    return  monthNames[(date.getMonth())] + " " + date.getDate() + ", " + date.getFullYear();
+}
 function GetTimeByZone(zone){
     switch(zone) {
         case 0: return ' 8:00 PDT';
         case 1: return ' 15:00 ST';
         default: return ' 15:00 ST';
     }
+}
+
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+  return Math.floor((utc1 - utc2) / (1000 * 60 * 60 * 24));
 }
 
