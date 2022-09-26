a60323ed55609bd20fbbe448b9d8af9c66f8da81
Mark <metalwolfmk@gmail.com>
2022-09-26 21:11:30 +0700

    t2

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ function GetDatePeriod() {
 }
 
 function formatDateOld(date) {
-    return date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate();
+    return date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
