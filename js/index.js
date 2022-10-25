4a6aa92a50d2002771794d1eaf981d412631d551
Mark <metalwolfmk@gmail.com>
2022-10-25 20:05:41 +0700

    t

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 /*jslint browser: true*/ /*global  $*/
-$(document).ready(function() {
+$(document).ready(function () {
     var data = GetDatePeriod();
     $('#entryPeriod').text(data[0])
     $('#resultsPeriod').text(data[1])
-    $("#countdown").countdown(data[2], function(event) {
-    $(this).text(data[3] +
-      event.strftime('%D days %H:%M:%S')
-    );
-    if(event.elapsed) {
-        $(this).text('Error (Wrong cycle in settings)');
-    }
-  });
-});  
+    $("#countdown").countdown(data[2], function (event) {
+        $(this).text(data[3] +
+            event.strftime('%D days %H:%M:%S')
+        );
+        if (event.elapsed) {
+            $(this).text('Error (Wrong cycle in settings)');
+        }
+    });
+});
 
 function GetDatePeriod() {
     var currentDate = new Date(); // Local DateTime
     var dateAnchor = new Date("October 8, 2022, 15:00:00 GMT+0000"); // 2022, 8, 11, 15, 0, 0
-    var currentCycle = Math.floor(dateDiffInDays(currentDate, dateAnchor)/9); // FIX
+    var currentCycle = Math.floor(dateDiffInDays(currentDate, dateAnchor) / 9); // FIX
     console.log(dateDiffInDays(currentDate, dateAnchor));
-    console.log(dateDiffInDays(currentDate, dateAnchor)/9);
-    dateAnchor.setUTCDate(dateAnchor.getUTCDate() + currentCycle*9)
+    console.log(dateDiffInDays(currentDate, dateAnchor) / 9);
+    dateAnchor.setUTCDate(dateAnchor.getUTCDate() + currentCycle * 9)
     var dateStart = new Date(dateAnchor.toUTCString());
     var dateEntry = new Date(dateAnchor.toUTCString());
     var dateResults = new Date(dateAnchor.toUTCString());
@@ -45,21 +45,21 @@ function GetDatePeriod() {
     var countdown = 0;
     var countdownPeriod = 'Entry';
     console.log('Entry: ' + currentDate.toUTCString() + '>' + dateStart.toUTCString());
-    if(currentDate.getTime() > dateStart.getTime() && currentDate.getTime() < dateEntry.getTime()) {
+    if (currentDate.getTime() > dateStart.getTime() && currentDate.getTime() < dateEntry.getTime()) {
         countdown = formatDateOld(dateEntry);
         countdownPeriod = 'Entry  period ends in ';
     }
 
     console.log('Result: ' + currentDate.toUTCString() + '>' + dateEntry.toUTCString() + ' && ' + currentDate.toUTCString() + '<' + dateResults.toUTCString());
-    if(currentDate.getTime() > dateEntry.getTime() && currentDate.getTime() < dateResults.getTime()) {
+    if (currentDate.getTime() > dateEntry.getTime() && currentDate.getTime() < dateResults.getTime()) {
         countdown = formatDateOld(dateResults);
         countdownPeriod = 'Results period ends in ';
     }
 
-    if(currentDate.getTime() < dateStart.getTime()) {
-        countdown = formatDateOld(dateEntry);
-        countdownPeriod = 'Results period ends in ';
-    }
+    // if (currentDate.getTime() < dateStart.getTime()) {
+    //     countdown = formatDateOld(dateEntry);
+    //     countdownPeriod = 'Results period ends in ';
+    // }
 
     return [
         ('' + strDateStart + GetTimeByZone() + ' ➜ ' + strDateEntry + GetTimeByZone() + ''),
@@ -76,10 +76,10 @@ function formatDateOld(date) {
 
 
 function formatDateNew(date) {
-    return  date.toLocaleString('default', { month: 'long', timeZone: 'UTC' }) + " " + date.getDate() + ", " + date.getFullYear();
+    return date.toLocaleString('default', { month: 'long', timeZone: 'UTC' }) + " " + date.getDate() + ", " + date.getFullYear();
 }
-function GetTimeByZone(zone){
-    switch(zone) {
+function GetTimeByZone(zone) {
+    switch (zone) {
         case 0: return ' 8:00 PDT';
         case 1: return ' 15:00 ST';
         default: return ' 15:00 ST';
@@ -88,10 +88,10 @@ function GetTimeByZone(zone){
 
 // a and b are javascript Date objects
 function dateDiffInDays(a, b) {
-  // Discard the time and time-zone information.
-  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+    // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
 
-  return Math.floor((utc1 - utc2) / (1000 * 60 * 60 * 24));
+    return Math.floor((utc1 - utc2) / (1000 * 60 * 60 * 24));
 }
 
